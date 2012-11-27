c49468d4e5f3d7742a330dc63774b501decdd97e
yanshuyuan <bupt.yanshuyuan@gmail.com>
2012-11-27 14:03:54 +0800

    change rename.sh

--- data/money/rename.sh
+++ data/money/rename.sh
@@ -1,11 +1,20 @@
 FROMDIR=parser
-SUBJECT=money_
+SUBJECT=`pwd | awk -F'/' {'print $NF'}`_
+
 for filename in `ls -l $FROMDIR | awk \{'print $8'\}`; do
     echo $FROMDIR/$SUBJECT$filename
-    mv $FROMDIR/$filename $FROMDIR/$SUBJECT$filename
+#    mv $FROMDIR/$filename $FROMDIR/$SUBJECT$filename
 done
-TRAINDIR=/home/administrator/cs/dataminer/train
-TESTDIR=/home/administrator/cs/dataminer/test
+
+TRAINDIR=../../train
+TESTDIR=../../test
+if [ ! -d $TRAINDIR ]; then
+    mkdir $TRAINDIR
+fi
+if [ ! -d $TESTDIR ]; then
+    mkdir $TESTDIR
+fi
+
 for filename in `ls -l $FROMDIR | head -200 | awk \{'print $8'\}`; do
     cp $FROMDIR/$filename $TESTDIR
 done
