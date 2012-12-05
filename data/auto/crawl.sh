a327ea42f3ae60f519b2fdb6747ffe124b8786f6
yanshuyuan <bupt.yanshuyuan@gmail.com>
2012-12-05 17:19:02 +0800

    Update data/auto/crawl.sh

--- data/auto/crawl.sh
+++ data/auto/crawl.sh
@@ -1,8 +1,8 @@
-#for url in `cat link.dat | grep 'html\$'`; do
-#    wget $url 
-#done
+for url in `cat link.dat | grep 'html\$'`; do
+    wget $url 
+done
 TO_DIR=url
 mkdir $TO_DIR
-for filename in `ls -l *.html | awk {'print $8'}`;do
+for filename in `ls -l *.html | awk {'print $NF'}`;do
     mv $filename $TO_DIR
 done
